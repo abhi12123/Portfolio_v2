10f998193cc0c76f76935f5e3ff2f3ee79245708
abhi12123 <vpofficial55@gmail.com>
2020-11-19 11:44:11 +0530

    added navbar, icons, links

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 AOS.init();
+const hamburger=document.querySelector('.hamburger');
+const hamburgerNavbar=document.querySelector('.hamburger-navbar')
+const closeBtn=document.querySelector('#close-btn')
+hamburger.addEventListener('click',()=>{
+    hamburgerNavbar.style.transform='translateX(0)';
+})
+closeBtn.addEventListener('click',()=>{
+    hamburgerNavbar.style.transform='translateX(-100%)';
+})
